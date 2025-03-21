3b70283f43a16a347efd5973e65301eb55d43b8a
Maxwell Jung <jungmaxwell@gmail.com>
2025-03-21 17:55:12 +0000

    chore: merge hw3 cache policies

--- configs/deprecated/example/se.py
+++ configs/deprecated/example/se.py
@@ -139,6 +139,8 @@ if "--ruby" in sys.argv:
 
 args = parser.parse_args()
 
+print(args.cpu_type, args.l1d_assoc, args.l1d_repl, args.l2_repl)
+
 multiprocesses = []
 numThreads = 1
 
@@ -177,6 +179,7 @@ else:
 
 
 (CPUClass, test_mem_mode, FutureClass) = Simulation.setCPUClass(args)
+CPUClass = CPUClass[0]
 CPUClass.numThreads = numThreads
 
 # Check -- do not allow SMT with multiple CPUs
